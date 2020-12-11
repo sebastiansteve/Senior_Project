377eccb47a8a69caf6c02e3d8b82f9567bcc1097
sebastiansteve <43971058+sebastiansteve@users.noreply.github.com>
2020-12-10 22:20:33 -0700

    Fixed titles

--- controllers/authControl.js
+++ controllers/authControl.js
@@ -101,7 +101,7 @@ exports.postLogin = (req, res, next) => {
                         req.session.user = user;
                         return req.session.save(err => {
                             console.log(err);
-                            res.redirect('/');
+                            res.redirect('/my-stuff');
                         });
                     }
                     req.flash('error', 'Invalid email or password.');
